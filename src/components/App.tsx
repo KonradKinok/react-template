19424ce89ce3a17a5cd923f23afbce6fe8f7a667
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2024-07-15 15:31:39 +0200

    15.07.2024

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,6 +4,7 @@ function App() {
     <>
       <h1>React Homework Template</h1>
       <h2>(Vite)</h2>
+      <h3>15.07.2024</h3>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import "./globalStyles/index.css"
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App.jsx";
+import App from "./components/App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
