15b42029b69929880e4e4d66375954844c288375
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2024-07-30 21:25:20 +0200

    30.07.2024

--- global.d.ts
+++ global.d.ts
@@ -2,3 +2,13 @@ declare module '*.scss' {
   const content: { [className: string]: string };
   export default content;
 }
+
+declare module '*.tsx' {
+    const content: any;
+    export default content;
+}
+
+declare module '*.jsx' {
+    const content: any;
+    export default content;
+}
