e263039814a46b4acd865d37259b9e8dad6d39c9
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2024-08-16 17:25:29 +0200

    Update functions.ts

--- src/globalFunctions/functions.ts
+++ src/globalFunctions/functions.ts
@@ -104,3 +104,14 @@ export function formatDate(dateStr: string):string {
   const year = date.getFullYear();
   return `${day}.${month}.${year}`;
 };
+
+export function formatDateAndHour(dateStr: string):string {
+  const date = new Date(dateStr);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Miesiące są indeksowane od 0
+  const year = date.getFullYear();
+  const hour = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  return `${day}.${month}.${year}y. ${hour}:${minutes}:${seconds}`;
+};
